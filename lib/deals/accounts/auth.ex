877088dc802433abeaf41eb7335a745d53a5a2e0
Matt Cree <matthew.cree@gmail.com>
2018-02-08 20:37:01 +0000

    Simplified template

--- lib/deals/accounts/auth.ex
+++ lib/deals/accounts/auth.ex
@@ -4,15 +4,15 @@ defmodule Deals.Accounts.Auth do
   def login(params, repo) do
     user = repo.get_by(User, email: String.downcase(params["email"]))
     case authenticate(user, params["password"]) do
-    true -> {:ok, user}
-    _    -> :error
+     true -> {:ok, user}
+      _    -> :error
     end
   end
   
   defp authenticate(user, password) do
     case user do
-    nil -> false
-    _   -> Encryption.validate_password(password, user.password_hash)
+      nil -> false
+      _   -> Encryption.validate_password(password, user.password_hash)
     end
   end
   
